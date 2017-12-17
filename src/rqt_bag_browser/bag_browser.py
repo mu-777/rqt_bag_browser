f66d2e807ec0c16f26eb8ded405f16d1c9011425
mu-777 <ryosuke.murata.7@gmail.com>
2017-12-17 12:59:50 +0900

    slim up

--- src/rqt_bag_browser/bag_browser.py
+++ src/rqt_bag_browser/bag_browser.py
@@ -202,15 +202,6 @@ class BagBrowserMainWidget(QMainWindow):
         self._widget.start()
         self.setCentralWidget(self._widget)
 
-    def save_settings(self, plugin_settings, instance_settings):
-        self._widget.save_settings(plugin_settings, instance_settings)
-
-    def restore_settings(self, plugin_settings, instance_settings):
-        self._widget.restore_settings(plugin_settings, instance_settings)
-
-    def shutdown_plugin(self):
-        self._widget.shutdown_plugin()
-
 
 # --------------------------------------------
 if __name__ == '__main__':
